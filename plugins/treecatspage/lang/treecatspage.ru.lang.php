5013e0af09a56d522bddfa42b808a6dd2e446fb5
sodium carbonate <webitproff@gmail.com>
2026-03-04 13:21:19 +0200

    Revise treecatspage.ru.lang.php with updated info
    Updated the Russian language file for Tree Cats Page plugin with new metadata and links.

--- plugins/treecatspage/lang/treecatspage.ru.lang.php
+++ plugins/treecatspage/lang/treecatspage.ru.lang.php
@@ -1,11 +1,22 @@
 <?php
+	
 /**
- * Russian Language File treecatspage.ru.lang.php for Tree Cats Page
+ * Tree Cats Page (tree of page categories anywhere)
+ * Plugin treecatspage for Cotonti 0.9.26, PHP 8.4+
+ * Filename: treecatspage.ru.lang.php 
+ * Purpose: Russian Language File
+ * Date: March 4Th, 2026
+ * Note: Correct counting + clickability of parent categories along with a drop-down list of child categories
+ * Source: https://github.com/webitproff/cot-treecatspage
+ * WepPage: https://abuyfile.com/ru/market/cotonti/plugs/plagin-tree-cats-page-dlya-cotonti-siena-0926
  *
  * @package treecatspage
- * @copyright (c) Cotonti
- * @license https://github.com/Cotonti/Cotonti/blob/master/License.txt
- */
+ * @version 2.2.27
+ * @author webitproff
+ * @copyright Copyright (c) webitproff 2026 | https://github.com/webitproff
+ * @license BSD
+ */  
+ 
 
 defined('COT_CODE') or die('Wrong URL.');
 
@@ -14,4 +25,5 @@ defined('COT_CODE') or die('Wrong URL.');
  */
 $L['info_name'] = 'Tree Cats Page';
 $L['info_desc'] = 'Дерево категорий для модуля страниц выводим в любом месте шаблона глобально.';
-$L['info_notes'] = "Вызов функции в шаблоне <code>{PHP|cot_treecatspage_build_structure_page_tree('', '')}</code>";
+
+$L['info_notes'] = '<a href="https://github.com/webitproff/cot-treecatspage/blob/main/README.md" target="_blank"><strong>README.md</strong></a> Вызов функции в шаблоне <code>{PHP|cot_treecatspage_build_structure_page_tree(\'\', \'\')}</code>';
